2ef8e6f17f9784892ec048c13fc92d0b8cc4317c
Ignacio Sosa <ignacio@mobilenik.com>
2025-07-05 23:17:10 -0300

    Initial commit

---
+++ finapp-ios/finapp-ios/finapp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  finapp_iosApp.swift
+//  finapp-ios
+//
+//  Created by Ignacio Sosa on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct finapp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
